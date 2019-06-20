e0c79f501c866238e43faa79eb15842235d4da89
Matthew Sneed <msneed@sitchai.com>
2019-06-19 23:43:30 -0400

    updates

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Lua
 
 These are some basic lua scripts.
+
+### Using Sockets
+You may get an error with ```local socket = require("socket")```
+
+So install the library via luarocks.
+```
+luarocks install luasocket
+```
--- server.lua
+++ server.lua
@@ -1,18 +1,18 @@
-
+-- Creae a new Socket
 local socket = require("socket")
-
 local server = assert(socket.bind("*",666))
-
 local ip, port = server:getsockname()
 
+-- Start/Loop Server
 while 1 do
+    -- Listen for connections..
     local client = server:accept()
     client:settimeout(10)
     local line, err = client:receive()
-
-    if not err then 
+    -- Send the incoming data back out
+    if not err then     
         client:send(line .. "\n") 
     end
-
+    -- Close connection
     client:close()
 end
