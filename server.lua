82cced1551c98585cff6b74bf93e4d221bf2680d
Matthew Sneed <msneed@sitchai.com>
2019-06-20 00:37:40 -0400

    server

--- server.lua
+++ server.lua
@@ -1,18 +1,35 @@
--- Creae a new Socket
-local socket = require("socket")
-local server = assert(socket.bind("*",666))
-local ip, port = server:getsockname()
+
+function serversocket(port)
+    -- Creae a new Server Socket
+    local socket = require("socket")
+    local server = assert(socket.bind("*",port))
+    local _, port = server:getsockname()
+
+    print("Server Listening Port: " .. tostring(port))
+
+    return server
+end
+
+local app = serversocket(8989)
 
 -- Start/Loop Server
 while 1 do
-    -- Listen for connections..
-    local client = server:accept()
-    client:settimeout(10)
-    local line, err = client:receive()
+
+    local client = app:accept() -- Listen for connections
+    client:settimeout(3) -- Disconnect after x seconds
+    local line, err = client:receive() 
+
     -- Send the incoming data back out
     if not err then     
         client:send(line .. "\n") 
     end
+
+    local peer_name = client:getpeername() -- Hostname
+    local stats = client:getstats() -- Number of bytes received
+
+    print(stats)
+
     -- Close connection
     client:close()
+
 end
