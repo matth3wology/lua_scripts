3623628162bb08b2e59c727e6c5f07bbedeb10aa
Matthew Sneed <msneed@sitchai.com>
2019-06-19 23:40:00 -0400

    socket server test.

---
+++ server.lua
@@ -0,0 +1,18 @@
+
+local socket = require("socket")
+
+local server = assert(socket.bind("*",666))
+
+local ip, port = server:getsockname()
+
+while 1 do
+    local client = server:accept()
+    client:settimeout(10)
+    local line, err = client:receive()
+
+    if not err then 
+        client:send(line .. "\n") 
+    end
+
+    client:close()
+end
